237dcccd98bc5427f12bca837abf6716e0a7aa9c
UIT-23521174 <23521174@gm.uit.edu.vn>
2024-11-09 12:48:06 +0700

    add Input() and Setup()

--- snake.cpp
+++ snake.cpp
@@ -7,6 +7,50 @@ void HideCursor() { // Ẩn con trỏ soạn thảo
     cursorInfo.bVisible = false;  // Ẩn con trỏ
     SetConsoleCursorInfo(consoleHandle, &cursorInfo);
 }
+
+void Setup() {
+    gameOver = false;
+    paused = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+    nTail = 0;
+    lastMoveTime = GetTickCount(); // Lấy thời gian hiện tại
+}
+
+void Input() {
+    if (_kbhit()) {
+        switch (_getch()) {
+        case 'a':
+            if (dir != RIGHT) dir = LEFT;
+            break;
+        case 'd':
+            if (dir != LEFT) dir = RIGHT;
+            break;
+        case 'w':
+            if (dir != DOWN) dir = UP;
+            break;
+        case 's':
+            if (dir != UP) dir = DOWN;
+            break;
+        case 'p':
+            paused = !paused;
+            if (paused == false) system("cls");
+            break;
+        case 'x':
+            cout << "Are you sure you want to quit? (y/n):";
+            char confirm;
+            confirm = _getch();
+            if (confirm == 'y' || confirm == 'Y') gameOver = true;
+            system("cls");
+            break;
+        }
+    }
+}
+
 void Logic() {
     if (paused ) return;
     // Kiểm tra thời gian hiện tại so với thời điểm di chuyển trước
