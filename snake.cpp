cfb80e0d811b109732e91fdb20791de88cbcb49b
UIT-23521174 <23521174@gm.uit.edu.vn>
2024-11-09 12:17:44 +0700

    add Setup()

--- snake.cpp
+++ snake.cpp
@@ -7,3 +7,36 @@ void HideCursor() { // Ẩn con trỏ soạn thảo
     cursorInfo.bVisible = false;  // Ẩn con trỏ
     SetConsoleCursorInfo(consoleHandle, &cursorInfo);
 }
+
+void Setup() {
+    gameOver = false;
+    paused = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+    nTail = 0;
+    lastMoveTime = GetTickCount(); // Lấy thời gian hiện tại
+}
+
+void Draw() {
+
+}
+
+void Input() {
+
+}
+
+void Logic() {
+
+}
+
+void ShowInstructions() {
+
+}
+
+void GameOverScreen() {
+
+}
