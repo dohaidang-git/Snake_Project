c43b285be44bc22fabab8064054c6ea36c5533b6
Phan Nguyen Viet Bac <112311776+s19ma@users.noreply.github.com>
2024-11-09 12:32:40 +0700

    commit logic and draw functions

--- snake.cpp
+++ snake.cpp
@@ -7,3 +7,109 @@ void HideCursor() { // Ẩn con trỏ soạn thảo
     cursorInfo.bVisible = false;  // Ẩn con trỏ
     SetConsoleCursorInfo(consoleHandle, &cursorInfo);
 }
+void Logic() {
+    if (paused ) return;
+    // Kiểm tra thời gian hiện tại so với thời điểm di chuyển trước
+    DWORD currentTime = GetTickCount();
+    if (currentTime - lastMoveTime < 100)  // Rắn di chuyển mỗi 100ms
+        return;
+
+    lastMoveTime = currentTime;  // Cập nhật thời điểm di chuyển mới
+
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for (int i = 1; i < nTail; i++) {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
+    switch (dir) {
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    default:
+        break;
+    }
+
+    if (x < 0 || x >= width || y < 0 || y >= height) {
+        gameOver = true;
+    }
+
+    for (int i = 0; i < nTail; i++) {
+        if (tailX[i] == x && tailY[i] == y) {
+            gameOver = true;
+        }
+    }
+
+    if (x == fruitX && y == fruitY) {
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
+    }
+}
+
+void Draw() {
+    COORD cursorPosition;
+    cursorPosition.X = 0;
+    cursorPosition.Y = 0;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursorPosition);
+
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (j == 0)
+                cout << "#";
+
+            if (i == y && j == x)
+                cout << "O";
+            else if (i == fruitY && j == fruitX)
+                cout << "*";
+            else {
+                bool print = false;
+                for (int k = 0; k < nTail; k++) {
+                    if (tailX[k] == j && tailY[k] == i) {
+                        cout << "o";
+                        print = true;
+                    }
+                }
+                if (!print)
+                    cout << " ";
+            }
+
+            if (j == width - 1)
+                cout << "#";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+    maxx = max(maxx,score);
+    cout << "Score: " << score << endl;
+    cout << "Highest Score: " << maxx << endl;
+    if (paused)
+    {
+        cout << "Game paused - Press P to continue." << endl;
+    }
+}
