02193ed2082b587959a0cbc46560f53e9c7825ad
dohaidang-git <23520225@gm.uit.edu.vn>
2024-11-17 19:50:11 +0700

    Final Load Test

--- snake.h
+++ snake.h
@@ -10,14 +10,14 @@ bool gameOver, paused;
 int width, height, delay;
 int x, y, fruitX, fruitY, score;
 int maxx = 0;
-int tailX[100], tailY[100];
+int tailX[1000], tailY[1000];
 int nTail;
 enum Direction { STOP = 0, LEFT, RIGHT, UP, DOWN };
 Direction dir;
 DWORD lastMoveTime;
 
 void HideCursor(); // Ẩn con trỏ soạn thảo tránh gây khó nhìn (cái này viết rô)
-void SetupGameSettings();
+void SetupGameSettings(); // Cài đặt thiết lập chọn kích cỡ khung và tốc độ rắn
 void Setup(); //  Các thiết lập ban đầu về chiều dài, chiều rộng khung, toạ độ hiển thị của rắn, thức ăn, điểm, số đốt đuôi
 void Draw(); // Vẽ khung, điểm, nói chung là giao diện chơi
 void Input(); // Thiết lập các nút bấm vào trong trò chơi
